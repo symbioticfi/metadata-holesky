a8c347a2dc1bbd759cff811dc2674c71c31dda19
Sergey Kambalin <sergeyk@symbiotic.fi>
2024-12-19 12:55:16 +0600

    Fix messages

--- .github/workflows/scripts/messages.ts
+++ .github/workflows/scripts/messages.ts
@@ -2,14 +2,14 @@ export const notAllowedChanges = (files: string[]) =>
   `We detected changes in the pull request that are not allowed. Please, follow the [contribution guidelines](README.md).
 
   **Not allowed changes:**
-  ${files.map((file) => `- ${file}`).join('\n')},
+  ${files.map((file) => `- ${file}`).join('\n')}
 `;
 
 export const onlyOneEntityPerPr = (dirs: string[]) =>
   `It is not allowed to change more than one entity in a single pull request. Please, follow the [contribution guidelines](README.md).
 
   **Changed entities:**
-  ${dirs.map((file) => `- ${file}`).join('\n')},
+  ${dirs.map((file) => `- ${file}`).join('\n')}
 `;
 
 export const invalidStructure = (entityDir: string, files: string[]) =>
@@ -17,5 +17,5 @@ export const invalidStructure = (entityDir: string, files: string[]) =>
 
   **Current structure:**
   - ${entityDir}
-    ${files.map((file) => `- ${file}`).join('\n')},
+    ${files.map((file) => `- ${file}`).join('\n')}
 `;
