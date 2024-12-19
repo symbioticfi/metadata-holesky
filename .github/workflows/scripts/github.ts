231cb5afb06f2523d2ee9f3b53a39407f418b5eb
Sergey Kambalin <sergeyk@symbiotic.fi>
2024-12-19 11:53:48 +0600

    Add github token env

--- .github/workflows/scripts/github.ts
+++ .github/workflows/scripts/github.ts
@@ -1,7 +1,13 @@
 import * as core from '@actions/core';
 import * as github from '@actions/github';
 
-const token = core.getInput('github-token');
+const token = process.env.GITHUB_TOKEN;
+
+if (!token) {
+  core.setFailed('GITHUB_TOKEN env variable is required');
+  process.exit(1);
+}
+
 const octokit = github.getOctokit(token);
 
 export const addComment = async (body: string) => {
@@ -9,3 +15,11 @@ export const addComment = async (body: string) => {
 
   await octokit.rest.issues.createComment({ owner, repo, issue_number: number, body });
 };
+
+export const run = async (command: () => Promise<void>) => {
+  try {
+    await command();
+  } catch (error) {
+    core.setFailed(error.message);
+  }
+}
--- .github/workflows/scripts/validate-fs.ts
+++ .github/workflows/scripts/validate-fs.ts
@@ -2,87 +2,83 @@ import path from 'path';
 import fs from 'fs';
 import * as core from '@actions/core';
 import * as github from './github';
+import * as messages from './messages';
 
 const addressRegex = /^0x[a-fA-F0-9]{40}$/;
 const allowedTypes = ['vaults', 'operators', 'networks', 'tokens'];
 const allowedFiles = ['info.json', 'logo.png'];
 const changedFiles = process.argv.slice(2);
 
-const notAllowed = new Set<string>();
-const entityDirs = new Set<string>();
+github.run(async () => {
+  const notAllowed = new Set<string>();
+  const entityDirs = new Set<string>();
 
-for (const filePath of changedFiles) {
-  const dir = path.dirname(filePath);
-  const [type, address, fileName] = filePath.split(path.sep);
-  const isValid =
-    allowedTypes.includes(type) && addressRegex.test(address) && allowedFiles.includes(fileName);
+  for (const filePath of changedFiles) {
+    const dir = path.dirname(filePath);
+    const [type, address, fileName] = filePath.split(path.sep);
+    const isValid =
+      allowedTypes.includes(type) && addressRegex.test(address) && allowedFiles.includes(fileName);
 
-  if (isValid) {
-    entityDirs.add(dir);
-  } else {
-    notAllowed.add(filePath);
+    if (isValid) {
+      entityDirs.add(dir);
+    } else {
+      notAllowed.add(filePath);
+    }
   }
-}
 
-/**
- * Validate that there are only allowed changes
- */
-if (notAllowed.size) {
-  github.addComment(
-    [
-      `We detected changes in the pull request that are not allowed. Please, follow the [contribution guidelines](README.md).`,
-      '**Not allowed changes:**',
-      [...notAllowed].map((file) => `- ${file}`).join('\n'),
-    ].join('\n')
-  );
+  /**
+   * Validate that there are only allowed changes
+   */
+  if (notAllowed.size) {
+    await github.addComment(messages.notAllowedChanges([...notAllowed]));
 
-  core.error(
-    `The pull request includes changes outside the allowed directories:\n ${[...notAllowed].join(
-      ', '
-    )}`
-  );
-  process.exit(1);
-}
+    throw new Error(
+      `The pull request includes changes outside the allowed directories:\n ${[...notAllowed].join(
+        ', '
+      )}`
+    );
+  }
 
-/**
- * Validate that only one entity is changed per pull request
- */
-if (entityDirs.size > 1) {
-  core.error('Several entities are changed in one pull request');
+  /**
+   * Validate that only one entity is changed per pull request
+   */
+  if (entityDirs.size > 1) {
+    await github.addComment(messages.onlyOneEntityPerPr([...entityDirs]));
 
-  process.exit(1);
-}
+    throw new Error('Several entities are changed in one pull request');
+  }
 
-const [entityDir] = entityDirs;
-const existingFiles = fs.readdirSync(entityDir);
+  const [entityDir] = entityDirs;
+  const existingFiles = await fs.promises.readdir(entityDir);
 
-const [metadataPath, logoPath] = allowedFiles.map((name) => {
-  return existingFiles.includes(name) ? path.join(entityDir, name) : undefined;
+  const [metadataPath, logoPath] = allowedFiles.map((name) => {
+    return existingFiles.includes(name) ? path.join(entityDir, name) : undefined;
+  });
+
+  const [isMetadataChanged, isLogoChanged] = allowedFiles.map((name) => {
+    return changedFiles.some((file) => path.basename(file) === name);
+  });
+
+  /**
+   * Validate that metadata present in the entity folder.
+   */
+  if (!metadataPath) {
+    await github.addComment(messages.invalidStructure(entityDir, existingFiles));
+
+    throw new Error('`info.json` is not found in the entity folder');
+  }
+
+  /**
+   * Send metadata to the next validation step only if the file was changed and exists.
+   */
+  if (isMetadataChanged && metadataPath) {
+    core.setOutput('metadata', metadataPath);
+  }
+
+  /**
+   * Send logo to the next validation step only if the file was changed and exists.
+   */
+  if (isLogoChanged && logoPath) {
+    core.setOutput('logo', logoPath);
+  }
 });
-
-const [isMetadataChanged, isLogoChanged] = allowedFiles.map((name) => {
-  return changedFiles.some((file) => path.basename(file) === name);
-});
-
-/**
- * Validate that metadata present in the entity folder.
- */
-if (!metadataPath) {
-  core.error('`info.json` is not found in the entity folder');
-
-  process.exit(1);
-}
-
-/**
- * Send metadata to the next validation step only if the file was changed and exists.
- */
-if (isMetadataChanged && metadataPath) {
-  core.setOutput('metadata', metadataPath);
-}
-
-/**
- * Send logo to the next validation step only if the file was changed and exists.
- */
-if (isLogoChanged && logoPath) {
-  core.setOutput('logo', logoPath);
-}
