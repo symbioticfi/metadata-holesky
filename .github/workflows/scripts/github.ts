a56de39750efff492f9aaedb8701af5f0bb80b90
Sergey Kambalin <sergeyk@symbiotic.fi>
2025-01-14 15:43:20 +0600

    Add GitHub REST API version header

--- .github/workflows/scripts/github.ts
+++ .github/workflows/scripts/github.ts
@@ -2,6 +2,9 @@ import * as core from '@actions/core';
 import * as github from '@actions/github';
 
 const token = process.env.GITHUB_TOKEN;
+const headers = {
+  'X-GitHub-Api-Version': '2022-11-28',
+};
 
 if (!token) {
   core.setFailed('GITHUB_TOKEN env variable is required');
@@ -15,17 +18,17 @@ export type ReviewComment = {
   body: string;
   line?: number;
   position?: number;
-}
+};
 
 export type Review = {
   body?: string;
   comments?: ReviewComment[];
-}
+};
 
 export const addComment = async (body: string) => {
   const { owner, repo, number } = github.context.issue;
 
-  await octokit.rest.issues.createComment({ owner, repo, issue_number: number, body });
+  await octokit.rest.issues.createComment({ owner, repo, issue_number: number, body, headers });
 };
 
 export const addReview = async (review: Review) => {
@@ -36,6 +39,7 @@ export const addReview = async (review: Review) => {
     repo,
     pull_number: number,
     event: 'COMMENT',
+    headers,
     ...review,
   });
 };
